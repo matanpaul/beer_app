79c599973fda891d3a638576d477a0ef058483b4
MatanPaul <matan@gumroad.com>
2013-11-11 15:12:00 -0800

    beer app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeerPong::Application.config.secret_key_base = 'e0311d31d659a9bd2424f0f89c69755e213bd1c676d10972643f58b05bdc3741e48a708959dd2118a1e8fc6b2041d71fddf0ed61eed16bf6293b285c5ad762ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeerPong::Application.config.session_store :cookie_store, key: '_beer_pong_session'
